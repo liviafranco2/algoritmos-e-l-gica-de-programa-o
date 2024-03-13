c73d1bba242ad450f335325a46d1f51c4a8d0a05
Lívia Franco <105373380+liviafranco2@users.noreply.github.com>
2024-03-13 19:05:08 -0300

    Add files via upload
    atividade: encontre a solução ao problema:
           Pergunte o nome do usuário e escreva a mensagem:
           "Olá, [nome do usuário]" 
    
           executar funções(prompt),  variaveis, concatenação , tipo de dado string

---
+++ script.js
@@ -0,0 +1,17 @@
+   // alert("agora eu sei programar em Js") aprendi a fazer um alert em js
+   /* 
+       atividade: encontre a solução ao problema:
+       Pergunte o nome do usuário e escreva a mensagem:
+       "Olá, [nome do usuário]" 
+
+       executar funções(prompt),  variaveis, concatenação , tipo de dado string
+   */
+
+       let nome = prompt("Qual o seu nome?")
+       alert("Olá, nome" + nome)
+
+      
+       
+       
+       
+       
